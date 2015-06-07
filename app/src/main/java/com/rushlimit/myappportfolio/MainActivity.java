60d84e37a4508d41de7f95d8538026e33a1b1f5b
Mohammed Amin <silvertounge79@gmail.com>
2015-06-07 18:03:26 -0400

    Changed minimum SDK from 19 to 15 to target larger audience Used android:onClick (xml) instead of listeners in MainActivity

--- app/src/main/java/com/rushlimit/myappportfolio/MainActivity.java
+++ app/src/main/java/com/rushlimit/myappportfolio/MainActivity.java
@@ -2,8 +2,6 @@ package com.rushlimit.myappportfolio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -11,94 +9,15 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button spotify, scores, library, build, xyzReader, capstone;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        spotify = (Button) findViewById(R.id.spotifyStreamer);
-        scores = (Button) findViewById(R.id.scoresApp);
-        library = (Button) findViewById(R.id.libraryApp);
-        build = (Button) findViewById(R.id.buildItBigger);
-        xyzReader = (Button) findViewById(R.id.xyzReader);
-        capstone = (Button) findViewById(R.id.capstoneProject);
-
-        spotify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,
-                        "This button will launch my spotify streamer app!",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        scores.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,
-                        "This button will launch my scores app!",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        library.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,
-                        "This button will launch my library app!",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        build.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,
-                        "This button will launch my build it bigger app!",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        xyzReader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,
-                        "This button will launch my xyz reader app!",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        capstone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,
-                        "This button will launch my capstone app!",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void buttonMessage(View v) {
+        Toast.makeText(this,
+                "This button will launch my app: " + ((Button) v).getText() + "!",
+                Toast.LENGTH_SHORT).show();
     }
 }
